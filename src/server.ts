3b187b4cdf14c2f390e52704926f291d8dff7869
Mohie <mohieddin.tanna@iproperty.com.my>
2023-03-21 07:42:56 +0800

    init commit

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express, { Express, Request, Response } from "express";
+import httpServerless from "serverless-http";
+import cors from "cors";
+import SampleRoutes from "./routes/sample-routes";
+
+const app: Express = express();
+
+// map port
+const port = 3000;
+
+// handle and manage cross-origin calls
+app.use(cors());
+
+// handle and manage json requests
+app.use(express.json());
+
+// handle routes
+app.use("/healthcheck", async (req: Request, res: Response) => {
+  res.status(200).json({ message: "Hello Server" });
+});
+
+app.use("/api/samples", SampleRoutes);
+
+app.listen(port, () => {
+  console.log(`[Server]: I am running at http://localhost:${port}`);
+});
+
+// export express server as a serverless http app
+exports.handler = httpServerless(app);
